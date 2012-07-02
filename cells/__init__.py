db3c5a8c267d8495ed52ae7c8541e94b240c0fb7
Shane L <shane_lee@brown.edu>
2012-07-02 15:06:50 -0400

    Added the pyplot backend for Ubuntu

--- cells/__init__.py
+++ cells/__init__.py
@@ -1,2 +1,6 @@
 # This is an init file for this directory to make it a 'module'
 # from class_cell import Cell
+
+# also while we're here, let's just do the mpl backend
+import matplotlib as mpl
+mpl.use('Agg')
