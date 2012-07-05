851ab206046b4d8ee08aa79da55ae44113a11808
Shane L <shane_lee@brown.edu>
2012-07-05 14:24:31 -0400

    Added some comments from MS to class_cell.py. Changed name of pyramidal.py to L5_pyramidal.py (to differentiate from eventual L2_pyramidal.py)

--- cells/class_cell.py
+++ cells/class_cell.py
@@ -12,17 +12,20 @@ class Cell():
     # this connects an instance of Cell() to a postsynaptic target
     def connect_to_target(self, postsyn):
         # event generated at _ref_v, to postsyn where delivered
+        # netconobj = new NetCon(source section, target section, [threshold, delay, weight])
         nc = h.NetCon(self.soma(0.5)._ref_v, postsyn, sec=self.soma)
 
-        # event threshold, arbitrarily chosen for now
+        # event threshold, arbitrarily chosen for now (default is +10)
         nc.threshold = -10
         return nc
 
-    # define shape of soma
+    # define shape of soma -- this function is needed for gui representation of cell
     # need to find out whether or not we need to call h.define_shape() explicitly!!
     def shape_soma(self, L_soma):
         # self.soma.push()
         h.pt3dclear(sec=self.soma)
+
+        # h.ptdadd(x, y, z, diam) -- if this function is run, clobbers self.soma.diam set above
         h.pt3dadd(0, 0, 0, 1, sec=self.soma)
         h.pt3dadd(0, L_soma, 0, 1, sec=self.soma)
         # h.pop_section()
