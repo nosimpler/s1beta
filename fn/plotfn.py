f3ee707ce4d94bfba10507380a8ad4793c1f3b49
Shane L <shane_lee@brown.edu>
2012-10-30 16:55:31 -0400

    1.2.21 - Added an attempt to make plot truly asynchronous

--- fn/plotfn.py
+++ fn/plotfn.py
@@ -1,8 +1,8 @@
 # plotfn.py - pall and possibly other plot routines
 #
-# v 1.2.18
-# rev 2012-10-26 (SL: using pkernel to parallelize plotting)
-# last major: (SL: created from s1run.py)
+# v 1.2.21
+# rev 2012-10-30 (SL: attempting to make this truly asynchronous)
+# last major: (SL: using pkernel to parallelize plotting)
 
 from pdipole import pdipole
 from spec import MorletSpec
@@ -23,6 +23,13 @@ def pkernel(ddir, file_spk, file_dpl, gid_dict, tstop):
     pdipole(file_dpl, dfig_dpl)
     MorletSpec(file_dpl, dfig_spec)
 
+    return 0
+
+# r is the value returned by pkernel
+# this is sort of a dummy function
+def cb(r):
+    pass
+
 # plot function - this is sort of a stop-gap and shouldn't live here, really
 def pall(ddir, gid_dict, tstop):
     # all fig stuff needs to be moved
@@ -34,20 +41,20 @@ def pall(ddir, gid_dict, tstop):
     dpl_list = fio.file_match(ddir.fileinfo, 'dipole')
     spk_list = fio.file_match(ddir.fileinfo, 'spikes')
 
-    for file_spk in spk_list:
-        # spikefn.spikes_from_file(net.gid_dict, file_spk)
-        praster(gid_dict, tstop, file_spk, dfig_spk)
+    # for file_spk in spk_list:
+    #     # spikefn.spikes_from_file(net.gid_dict, file_spk)
+    #     praster(gid_dict, tstop, file_spk, dfig_spk)
 
-    for file_dpl in dpl_list:
-        # Plot dipole data
-        pdipole(file_dpl, dfig_dpl)
+    # for file_dpl in dpl_list:
+    #     # Plot dipole data
+    #     pdipole(file_dpl, dfig_dpl)
 
-        # Morlet analysis
-        MorletSpec(file_dpl, dfig_spec)
+    #     # Morlet analysis
+    #     MorletSpec(file_dpl, dfig_spec)
 
     pl = Pool()
     for file_spk, file_dpl in it.izip(spk_list, dpl_list):
-        pl.apply_async(pkernel, (ddir, file_spk, file_dpl, gid_dict, tstop))
+        pl.apply_async(pkernel, (ddir, file_spk, file_dpl, gid_dict, tstop), callback=cb)
 
     pl.close()
     pl.join()
